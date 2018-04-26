bb672cfe912e853e3aa4803db57ec5e14d4acb8a
Reinier Sanders <34060491+reiniersanders@users.noreply.github.com>
2018-04-26 12:23:41 +0200

    Add files via upload

---
+++ assignment9/NotSym.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment.pkg9;
+
+/**
+ *
+ * @author reiniersanders
+ */
+public enum NotSym implements Form{
+    NotSym( "<>" );
+    
+    private String string;
+    
+    private NotSym(String string){
+        this.string = string;
+    }
+    
+    @Override
+    public Form getId(){
+        return this;
+    }
+    
+    @Override
+    public boolean accept(FormVisitor v){
+        return v.visit(this);
+    }
+}
