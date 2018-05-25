a903e804619abc597f14c4dd8a580eaf2f0b3c64
Reinier Sanders <34060491+reiniersanders@users.noreply.github.com>
2018-05-25 12:39:41 +0200

    Update MergeSort.java

--- Assignment13/MergeSort.java
+++ Assignment13/MergeSort.java
@@ -1,73 +1,69 @@
-package assignment13;
+package assignment.pkg13;
 
 import java.util.Arrays;
 
 /**
- *
- * @author pieterkoopman
+ * @author Reinier Sanders  s4335422
+ * @author Laurens Kubat    s4626249
  */
 public class MergeSort {
-  /**
-   * sort the given array in O(N log N) time
-   * The array is split in two parts of equal size. 
-   * These parts are sort recursively and merged.
-   * @param array 
-   */
-    
-    
+    /**
+    * sort the given array in O(N log N) time
+    * The array is split in two parts of equal size. 
+    * These parts are sort recursively and merged.
+    * @param toSort 
+    */
+
     public void multiMergeSort(int[] toSort) {
-            if (toSort.length < 1000) {
-                sort(toSort);
-            } else {
-                int [] firstHalf = Arrays.copyOf(toSort, toSort.length / 2);
-                int [] secondHalf =  Arrays.copyOfRange(toSort, toSort.length / 2, toSort.length);
-                MultiMergeSort m1 = new MultiMergeSort(firstHalf);
-                MultiMergeSort m2 = new MultiMergeSort(secondHalf);
-                Thread t1 = new Thread(m1);
-                Thread t2 = new Thread(m2);
-                t1.start();
-                t2.start();
-                try {
-                    t1.join();
-                    t2.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                merge(firstHalf, secondHalf, toSort);
-                for (int i = 0; i < toSort.length; i++) {
-                    System.out.println(toSort[i]);
-                } 
+        if (toSort.length < 1000) {
+            sort(toSort);
+        } else {
+            int [] firstHalf = Arrays.copyOf(toSort, toSort.length / 2);
+            int [] secondHalf =  Arrays.copyOfRange(toSort, toSort.length / 2, toSort.length);
+            MultiMergeSort m1 = new MultiMergeSort(firstHalf);
+            MultiMergeSort m2 = new MultiMergeSort(secondHalf);
+            Thread t1 = new Thread(m1);
+            Thread t2 = new Thread(m2);
+            t1.start();
+            t2.start();
+            try {
+                t1.join();
+                t2.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+            merge(m1.getArray(), m2.getArray(), toSort);
+        }
     }
     
-  public static void sort(int [] array) {
-    if (array.length > 1) {
-      int [] firstHalf = Arrays.copyOf(array, array.length / 2);
-      sort(firstHalf);
-      int [] secondHalf = Arrays.copyOfRange(array, array.length / 2, array.length);
-      sort(secondHalf);
-      merge(firstHalf, secondHalf, array);
+    public static void sort(int [] array) {
+        if (array.length > 1) {
+          int [] firstHalf = Arrays.copyOf(array, array.length / 2);
+          sort(firstHalf);
+          int [] secondHalf = Arrays.copyOfRange(array, array.length / 2, array.length);
+          sort(secondHalf);
+          merge(firstHalf, secondHalf, array);
+        }
     }
-  }
-  /**
-   * merge two sorted arrays: O(N)
-   * @param part1 a sorted array
-   * @param part2 a sorted array
-   * @param dest  destination, length must be >= part1.length + part2.length
-   */
-  public static void merge(int [] part1, int [] part2, int dest[]) {
-    int part1Index = 0, part2Index = 0, destIndex = 0;
-    while (part1Index < part1.length && part2Index < part2.length) {
-      if (part1[part1Index] < part2[part2Index])
-        dest[destIndex ++] = part1[part1Index ++];
-      else
-        dest[destIndex ++] = part2[part2Index ++];
-    }
-    // copy elements when at most one of the parts contains elements
-    while (part1Index < part1.length)
-      dest[destIndex ++] = part1[part1Index ++];
-    while (part2Index < part2.length)
-      dest[destIndex ++] = part2[part2Index ++];
+    /**
+    * merge two sorted arrays: O(N)
+    * @param part1 a sorted array
+    * @param part2 a sorted array
+    * @param dest  destination, length must be >= part1.length + part2.length
+    */
+    public static void merge(int [] part1, int [] part2, int dest[]) {
+        int part1Index = 0, part2Index = 0, destIndex = 0;
+        while (part1Index < part1.length && part2Index < part2.length) {
+          if (part1[part1Index] < part2[part2Index])
+            dest[destIndex ++] = part1[part1Index ++];
+          else
+            dest[destIndex ++] = part2[part2Index ++];
+        }
+        // copy elements when at most one of the parts contains elements
+        while (part1Index < part1.length)
+          dest[destIndex ++] = part1[part1Index ++];
+        while (part2Index < part2.length)
+          dest[destIndex ++] = part2[part2Index ++];
   }
 
   /**
