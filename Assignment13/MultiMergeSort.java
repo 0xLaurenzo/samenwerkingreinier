e14de0d279344427d0f054cff3bcf0cfc6533671
Reinier Sanders <34060491+reiniersanders@users.noreply.github.com>
2018-05-25 12:40:48 +0200

    Update MultiMergeSort.java

--- Assignment13/MultiMergeSort.java
+++ Assignment13/MultiMergeSort.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package assignment13;
-
-import java.util.Arrays;
-import java.util.HashMap;
+package assignment.pkg13;
 
 /**
- *
- * @author Laurens
+ * @author Reinier Sanders  s4335422
+ * @author Laurens Kubat    s4626249
  */
 public class MultiMergeSort implements Runnable {
 
