2daefceb36fed9136f45c6e9efba616147ccd8b7
Reinier Sanders <34060491+reiniersanders@users.noreply.github.com>
2018-05-25 12:40:20 +0200

    Update MergeSortTest.java

--- Assignment13/MergeSortTest.java
+++ Assignment13/MergeSortTest.java
@@ -1,31 +1,29 @@
-package assignment13;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+package assignment.pkg13;
 
 import java.util.Random;
 
 /**
- *
- * @author Laurens
+ * @author Reinier Sanders  s4335422
+ * @author Laurens Kubat    s4626249
  */
 public class MergeSortTest {
     
     public static void main(String[] args) {
-        int[] data = new int[10000];
         Random random = new Random();
+        int[] data = new int[10000];
         for (int i = 0; i < data.length; i++) {
             data[i] = random.nextInt(10000);
         }
         MergeSort sort = new MergeSort();
+        long timeStart = System.currentTimeMillis();
         sort.multiMergeSort(data);
+        long timeEnd = System.currentTimeMillis();
+        long timeDif = timeEnd - timeStart;
         for (int i = 0; i < data.length; i++) {
             System.out.println(data[i]);
-        }        
+        }
+        System.out.println();
+        int processors = Runtime.getRuntime().availableProcessors();
+        System.out.println("Array sorted in " + timeDif + " milliseconds, using " + processors + " processors.");
     }
-    
 }
