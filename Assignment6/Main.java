04c545a893ba6bc13270bafd1383f3ba06befde3
Reinier Sanders <34060491+reiniersanders@users.noreply.github.com>
2018-06-15 17:54:52 +0200

    Update Main.java

--- Assignment6/Main.java
+++ Assignment6/Main.java
@@ -1,4 +1,4 @@
-import java.util.List;
+package assignment6;
 
 /*
 Reinier Sanders     s4335422
@@ -12,10 +12,9 @@ Laurens Kubat       s4626249
 public class Main
 {
     public static void main(String[] args) {
-        int [] game = {2,1,4, 8,3,6, 9,5,7};
+        int [] game = {2,1,4, 8,3,6, 9,7,5};
 
         SlidingGame s = new SlidingGame (game);
-        System.out.println(s);
         Solver solver = new Solver(s);
         System.out.println(solver.solve());
     }
