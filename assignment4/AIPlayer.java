f9f0aeb0e4e2c362982dc4880f23b621cf4c58d5
Reinier Sanders <34060491+reiniersanders@users.noreply.github.com>
2018-06-18 14:14:42 +0200

    Update AIPlayer.java

--- assignment4/AIPlayer.java
+++ assignment4/AIPlayer.java
@@ -5,39 +5,71 @@
  */
 package assignment4;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
- * @author Laurens
- *
+ * @author  Laurens Kubat   s4626249
+ * @author  Reinier Sanders s4335422
+ */
 public class AIPlayer implements Player {
-
+    private String name;
+    private String color;
+    private Board board;
+    
     public AIPlayer() {
+        getNameFromUser();
+        getColorFromUser();
+    }
+    
+    @Override
+    public int getY() {
+        Random rand = new Random();
+        return rand.nextInt(3);
     }
 
     @Override
-    public void play() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int getX() {
+        Random rand = new Random();
+        return rand.nextInt(3);
     }
 
     @Override
     public String getName() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.name;
     }
 
     @Override
     public void setName(String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.name = name;
     }
 
     @Override
     public String getColor() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.color;
     }
-
+    
     @Override
     public void setColor(String color) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.color = color;
+    }
+    
+    public void getNameFromUser(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the player's name");
+        setName(scan.nextLine());
+    }
+    
+    public void getColorFromUser(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter " + name + "'s symbol (X/O)");
+        String color = Character.toString(scan.next().charAt(0));
+        switch(color) {
+            case "X" : setColor("X"); break;
+            case "O" : setColor("O"); break;
+            default : System.out.println("Please enter only X or O"); getColorFromUser();
+        }
     }
     
 }
-*/
